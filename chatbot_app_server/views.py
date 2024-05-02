24c6b78eb4715ad552979bb2a2eca0f6bfd6f44b
Kyle Brandt <knbrandt47@gmail.com>
2024-05-01 21:25:36 -0400

    committing project

---
+++ chatbot_app_server/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')  # Redirect to a home page or another appropriate page
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Redirect to a home page or another appropriate page
+    else:
+        form = AuthenticationForm()
+    return render(request, 'chatbot_app/login.html', {'form': form})
